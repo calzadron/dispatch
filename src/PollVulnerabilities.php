8ee878ee5208fac6824607e51de798d6ae534586
calzadron <pete@null-set.com>
2016-08-09 08:02:07 -0400

    Update VulnerabilityPoller handler to allow MatchHandler setting

--- src/PollVulnerabilities.php
+++ src/PollVulnerabilities.php
@@ -5,9 +5,17 @@ namespace dispatch\core;
 $app = __DIR__;
 
 require_once $app.'/interfaces/VulnerabilityPoller.php';
+require_once $app.'/interfaces/MatchHandler.php';
 
 use dispatch\core\interfaces\VulnerabilityPoller;
+use dispatch\core\interfaces\MatchHandler;
 
 class PollVulnerabilities implements VulnerabilityPoller
 {
+	/**
+	 * @param dispatch\core\interfaces\MatchHandler $matchHandler
+	 */
+	public function addMatchHandler(MatchHandler $matchHandler)
+	{
+	}
 }
