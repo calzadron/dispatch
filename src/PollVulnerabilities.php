4d54744e4e32ba4355191a3b9e1c432a74122552
calzadron <pete@null-set.com>
2016-08-12 07:48:58 -0400

    Add addVulnerabilityMatcher method to VulnerabilityPoller

--- src/PollVulnerabilities.php
+++ src/PollVulnerabilities.php
@@ -7,10 +7,12 @@ $app = __DIR__;
 require_once $app.'/interfaces/VulnerabilityPoller.php';
 require_once $app.'/interfaces/MatchHandler.php';
 require_once $app.'/interfaces/VulnerabilityDataSource.php';
+require_once $app.'/interfaces/VulnerabilityMatcher.php';
 
 use dispatch\core\interfaces\VulnerabilityPoller;
 use dispatch\core\interfaces\MatchHandler;
 use dispatch\core\interfaces\VulnerabilityDataSource;
+use dispatch\core\interfaces\VulnerabilityMatcher;
 
 class PollVulnerabilities implements VulnerabilityPoller
 {
@@ -21,8 +23,18 @@ class PollVulnerabilities implements VulnerabilityPoller
 	{
 	}
 
+	/**
+	 * @param dispatch\core\interfaces\VulnerabilityDataSource $dataSource
+	 */
 	public function addVulnerabilityDataSource(
 		VulnerabilityDataSource $dataSource
 	) {
+	}
+
+	/**
+	 * @param dispatch\core\interfaces\VulnerabilityMatcher $matcher
+	 */
+	public function addVulnerabilityMatcher(VulnerabilityMatcher $matcher)
+	{
 	}
 }
